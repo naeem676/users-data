a0bcabf23e260d2630edf14d7feb17e23908c25d
naeem uddin <ali.naeem939@gmail.com>
2020-08-26 13:18:03 +0600

    navbar added

--- src/compunents/Header.js
+++ src/compunents/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import './Header.css';
 
 const Header = () => {
+    const fontStyle={
+        color:'white', 
+        marginLeft:'150px',
+    }
     return (
-        <div>
-        <h1>this is Header</h1>
+        <div className="header">
+        <nav>
+        <div className="input">
+        <button>Search</button>
+        <input type="text" placeholder="Search"/><FontAwesomeIcon style={fontStyle} icon={faHome} />
+        <FontAwesomeIcon style={fontStyle} icon={faUsers} />
+        <FontAwesomeIcon style={fontStyle} icon={faBell} />
+        </div>
+        
+            
+        </nav>
+       
             
         </div>
     );
